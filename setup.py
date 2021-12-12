60a003e96b240616d8c869872fc2ca44ef387674
Sean Kugele <seankugele@gmail.com>
2021-12-12 11:15:32 -0600

    Adds setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ModifiedSchemaMechanism',
+      version='0.1',
+      description='A Modified Schema Mechanism',
+      author='Sean Kugele',
+      author_email='seankugele@gmail.com',
+      url='https://github.com/skugele/schema-mechanism',
+      license='MIT',
+      install_requires=[],
+)
