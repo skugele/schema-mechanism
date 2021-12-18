407cecd4a3a2c3c441bbd7cf0b31606695fb887e
Sean Kugele <seankugele@gmail.com>
2021-12-18 12:35:15 -0600

    Updates dependencies

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 
 from distutils.core import setup
 
-setup(name='ModifiedSchemaMechanism',
+setup(name='SchemaMechanism',
       version='0.1',
-      description='A Modified Schema Mechanism',
+      description='Schema Mechanism',
       author='Sean Kugele',
       author_email='seankugele@gmail.com',
       url='https://github.com/skugele/schema-mechanism',
       license='MIT',
-      install_requires=[],
+      install_requires=['numpy==1.21.4', 'scikit-learn==1.0.1'],
 )
