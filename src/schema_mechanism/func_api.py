7a54d64cdd86f1e2dcc9417f95f760607213d319
Sean Kugele <seankugele@gmail.com>
2022-01-13 06:23:03 -0600

    Adds make_assertions function to func_api

--- src/schema_mechanism/func_api.py
+++ src/schema_mechanism/func_api.py
@@ -1,9 +1,10 @@
-from typing import Union
+from typing import Iterable
+from typing import MutableSet
 
-from schema_mechanism.data_structures import SymbolicItem
-from schema_mechanism.data_structures import StateElement
 from schema_mechanism.data_structures import Item
 from schema_mechanism.data_structures import ItemAssertion
+from schema_mechanism.data_structures import StateElement
+from schema_mechanism.data_structures import SymbolicItem
 
 
 def create_item(state_element: StateElement) -> Item:
@@ -12,3 +13,7 @@ def create_item(state_element: StateElement) -> Item:
 
 def make_assertion(state_element: StateElement, negated: bool = False) -> ItemAssertion:
     return ItemAssertion(item=create_item(state_element), negated=negated)
+
+
+def make_assertions(state_elements: Iterable[StateElement], negated: bool = False) -> MutableSet[ItemAssertion]:
+    return set([ItemAssertion(item=create_item(se), negated=negated) for se in state_elements])
--- tests/test_func_api.py
+++ tests/test_func_api.py
@@ -3,10 +3,11 @@ import unittest
 from schema_mechanism.data_structures import ItemAssertion
 from schema_mechanism.data_structures import SymbolicItem
 from schema_mechanism.func_api import make_assertion
+from schema_mechanism.func_api import make_assertions
 
 
 class TestFunctionalApi(unittest.TestCase):
-    def test_assertion(self):
+    def test_make_assertion(self):
         # discrete item assertions
         v = '1234'
         ia = make_assertion(state_element=v, negated=True)
@@ -14,3 +15,31 @@ class TestFunctionalApi(unittest.TestCase):
         self.assertIsInstance(ia.item, SymbolicItem)
         self.assertEqual(v, ia.item.state_element)
         self.assertEqual(True, ia.negated)
+
+    def test_make_assertions(self):
+        n_items = 1000
+        ias = make_assertions(range(n_items))
+        self.assertEqual(n_items, len(ias))
+
+        for ia in ias:
+            # verify item types are ItemAssertion
+            self.assertIsInstance(ia, ItemAssertion)
+
+            # verify negated value (False) applied to all item assertions
+            self.assertFalse(ia.negated)
+
+        # verify all state elements in returned collection
+        self.assertTrue(all({make_assertion(se) in ias for se in range(n_items)}))
+
+        ias = make_assertions(range(n_items), negated=True)
+        self.assertEqual(n_items, len(ias))
+
+        for ia in ias:
+            # verify item types are ItemAssertion
+            self.assertIsInstance(ia, ItemAssertion)
+
+            # verify negated value (True) applied to all item assertions
+            self.assertTrue(ia.negated)
+
+        # verify all state elements in returned collection
+        self.assertTrue(all({make_assertion(se, negated=True) in ias for se in range(n_items)}))
